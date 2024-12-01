36602213feb187523e615cbf682af656312f8f2c
edsghot <edsghot@gmail.com>
2024-12-01 03:34:17 -0500

    feat: the architecture structure was added

---
+++ Configuration/Context/MySqlContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using jhedgostBK.Modules.User.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace jhedgostBK.Configuration.DataBase
+{
+    public class MySqlContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=jhedgost.com;Database=dbjhfjuv_jhedgostApi;User=dbjhfjuv_edsghot;Password=Repro321.;";
+
+            optionsBuilder.UseMySql(
+                connectionString,
+                new MySqlServerVersion(new Version(8, 0, 21))
+            );
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Cambié el nombre de la tabla a 'User'
+            modelBuilder.Entity<UserEntity>().ToTable("User");
+
+            // Aquí definimos que la clave primaria es 'Id'
+            modelBuilder.Entity<UserEntity>().HasKey(u => u.Id);
+
+            modelBuilder.Entity<UserEntity>()
+                .Property(u => u.Name)
+                .HasMaxLength(50)
+                .IsRequired(); // Ejemplo de cómo configurar propiedades de la entidad
+        }
+
+
+    }
+}
---
+++ Modules/User/Domain/Entity/UserEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace jhedgostBK.Modules.User.Domain.Entity
+{
+    public record UserEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
